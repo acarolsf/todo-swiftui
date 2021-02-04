03ad7560ca4fb10f9d531a50f26102643d860f99
Ana Carolina <cscarol20@gmail.com>
2021-02-04 18:14:54 -0300

    Initial Commit

---
+++ todo-app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo-app
+//
+//  Created by Ana Carolina on 04/02/21.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
